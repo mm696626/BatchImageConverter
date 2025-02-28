4fbf495c49a38039a9cb412aff41135cd5dbf1cd
Matt McCullough <mmccullough6899@gmail.com>
2025-02-28 07:34:10 -0500

    Add optional delete original files checkbox

--- src/Main.py
+++ src/Main.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 from tkinter import PhotoImage
 
 import batchConvert
+import os
 
 def browse_folder(entry_widget):
     folder_selected = filedialog.askdirectory()
@@ -14,7 +15,15 @@ def browse_folder(entry_widget):
 def convert_images():
     image_folder = image_folder_path.get()
     output_folder = output_folder_path.get()
+    delete_original = delete_checkbox_var.get()
     batchConvert.batch_convert(image_folder, output_folder)
+
+    if delete_original:
+        for filename in os.listdir(image_folder):
+            file_path = os.path.join(image_folder, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
     messagebox.showinfo("Done", "Conversion is Completed!")
 
 root = tk.Tk()
@@ -22,7 +31,7 @@ root.title("Wii Photo Channel Image Converter")
 icon = PhotoImage(file='images/photo-channel-icon.png')
 root.iconphoto(True, icon)
 
-image_folder_label = tk.Label(root, text="Image Folder:")
+image_folder_label = tk.Label(root, text="Image Folder")
 image_folder_label.grid(row=0, column=0, padx=10, pady=10)
 
 image_folder_path = tk.Entry(root, width=40)
@@ -42,5 +51,9 @@ output_folder_browse.grid(row=1, column=2, padx=10, pady=10)
 
 convert_button = tk.Button(root, text="Convert", command=convert_images)
 convert_button.grid(row=2, column=0, columnspan=3, pady=20)
+
+delete_checkbox_var = tk.BooleanVar()
+delete_checkbox = tk.Checkbutton(root, text="Delete Original Files After Conversion", variable=delete_checkbox_var)
+delete_checkbox.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
 
 root.mainloop()
